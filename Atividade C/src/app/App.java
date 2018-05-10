21c261df057a948eb4ce2b6d35d45cb75d3272dd
RoberthBSantos <fco.roberth@gmail.com>
2018-05-10 01:07:05 -0300

    Criando menu e adicionando metodo exibir highscores

--- Atividade C/src/app/App.java
+++ Atividade C/src/app/App.java
@@ -1,4 +1,44 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import model.Score;
+
 public class App {
+	
+	static List<Score> pontuacoes = new ArrayList<Score>();
+	
+	public static void main(String[] args) {
+		
+		menu();
+		
+	}
+	
+	public static void menu(){
+		int escolha = Integer.valueOf(JOptionPane.showInputDialog("		MENU \n 1- Jogar \n 2- Cadastrar palavra\n"
+				+ " 3- Cadastrar tema\n 4- HighScores\n 5- Sair"));
+		
+		switch (escolha) {
+		case 1:
+
+		case 2:
+			
+		case 3:
+			
+		case 4:
+			Score score = new Score();
+			JOptionPane.showMessageDialog(null,score.exibir_higscores(pontuacoes));
+			menu();
+			
+		case 5:
+			
+			break;
+		default:
+			break;
+		}
+		
+	}
 }
